8280d6f9988e07448541975d00c81c0f60feacf1
Michael Mallari <michael@michaelmallari.com>
2017-01-25 14:42:10 -0500

    Added notemplate theme files

---
+++ app/wp-content/themes/notemplate/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en-us">
+	<head>
+		<title></title>
+		<meta name="robots" CONTENT="noindex, nofollow">
+		<meta http-equiv="refresh" content="0;URL='http://www.michaelmallari.com'">    
+	</head>
+
+
+	<body>
+	</body>
+</html>
